a871e19bf8deab65214d037d5f03c886412a66fd
aliriogithub <alirio.hernandez@appgate.com>
2020-10-02 14:15:55 -0500

    basic auth

--- src/test/java/mentoring/tests/RestAssuredDemoTest.java
+++ src/test/java/mentoring/tests/RestAssuredDemoTest.java
@@ -5,7 +5,11 @@ import mentoring.entities.UpdateUserResponse;
 import mentoring.entities.UserBody;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -97,6 +101,25 @@ public class RestAssuredDemoTest {
                 .statusCode(200).extract().body().as(UpdateUserResponse.class);
 
         System.out.println(response.getUpdatedAt());
+    }
+
+
+    static Stream<Arguments> stringIntAndListProvider() {
+        return Stream.of(
+                Arguments.of("user","passwd"),
+                Arguments.of("alriio","contra")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringIntAndListProvider")
+    void testBasicAuthRestAssured(String user,String password) {
+        String url="https://httpbin.org";
+        given().log().all().baseUri(url).basePath("/basic-auth/{user}/{passwd}")
+                .pathParam("user",user).pathParam("passwd",password)
+                .auth().basic(user,password)
+                .log().all().when().get()
+                .then().log().all().statusCode(200);
     }
 
 
